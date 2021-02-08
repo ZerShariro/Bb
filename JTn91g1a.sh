9af93ed086092536dd03e0385ff27d0cafa6044b
ZerShi <41265484+ZerShariro@users.noreply.github.com>
2021-02-08 20:23:07 +0300

    Add files via upload

--- JTn91g1a.sh
+++ JTn91g1a.sh
@@ -1,33 +1,3 @@
-# Various bash bitcoin tools
-#
-# requires dc, the unix desktop calculator (which should be included in the
-# 'bc' package)
-#
-# This script requires bash version 4 or above.
-#
-# This script uses GNU tools.  It is therefore not guaranted to work on a POSIX
-# system.
-#
-# Copyright (C) 2013 Lucien Grondin (grondilu@yahoo.fr)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 if ((BASH_VERSINFO[0] < 4))
 then
     echo "This script requires bash version 4 or above." >&2
